4b9cab3e15a3ee46b5bb420f5e34a975e18cdc8b
pinbor <pinbor@iissy.com>
2023-06-29 14:20:02 +0800

    sftp

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.17
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-redsync/redsync/v4 v4.5.1
+	github.com/pkg/sftp v1.13.5
 	github.com/spf13/cobra v1.6.1
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 )
 
 require (
@@ -14,5 +16,7 @@ require (
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/kr/fs v0.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 )
---
+++ sftp/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
+
+//https://github.com/pkg/sftp
+//
+func main() {
+	sshConfig := &ssh.ClientConfig{
+		User: "root",
+		Auth: []ssh.AuthMethod{
+			ssh.Password("jc.123456"),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		ClientVersion:   "",
+		Timeout:         10 * time.Second,
+	}
+
+	//建立与SSH服务器的连接
+	sshClient, err := ssh.Dial("tcp", "192.168.1.234:22", sshConfig)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer sshClient.Close()
+
+	sftpClient, err := sftp.NewClient(sshClient)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	defer sftpClient.Close()
+
+	//获取当前目录
+	cwd, err := sftpClient.Getwd()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println("当前目录：", cwd)
+
+	//显示文件/目录详情
+	fi, err := sftpClient.Lstat(cwd)
+	log.Println(fi)
+
+	{
+		//上传文件(将本地file.dat文件通过sftp传到远程服务器)
+		remoteFileName := fmt.Sprintf("upload_by_sftp_%d.dat", rand.Int())
+		remoteFile, err := sftpClient.Create(sftp.Join(cwd, remoteFileName))
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		defer remoteFile.Close()
+
+		localFileName := "file.dat"
+		//打开本地文件file.dat
+		localFile, err := os.Open(localFileName)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		defer localFile.Close()
+
+		//本地文件流拷贝到上传文件流
+		n, err := io.Copy(remoteFile, localFile)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		//获取本地文件大小
+		localFileInfo, err := os.Stat(localFileName)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		log.Printf("文件上传成功[%s->%s]本地文件大小：%s，上传文件大小：%s", localFileName, remoteFileName, formatFileSize(localFileInfo.Size()), formatFileSize(n))
+
+		//计算文件MD5
+		//windows计算：certutil -hashfile .\file.dat MD5
+		//linux计算：md5sum upload_by_sftp_5577006791947779410.dat
+	}
+	{
+		//下载文件
+		//将远程服务器的/bin/bash文件下载到本地
+		remoteFileName := "/bin/bash"
+		remoteFile, err := sftpClient.Open(remoteFileName)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		defer remoteFile.Close()
+
+		localFileName := "local-bash"
+		localFile, err := os.Create(localFileName)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		defer localFile.Close()
+		n, err := io.Copy(localFile, remoteFile)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+
+		//获取远程文件大小
+		remoteFileInfo, err := sftpClient.Stat(remoteFileName)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		log.Printf("文件下载成功[%s->%s]远程文件大小：%s，下载文件大小：%s", remoteFileName, localFileName, formatFileSize(remoteFileInfo.Size()), formatFileSize(n))
+	}
+}
+
+// 字节的单位转换 保留两位小数
+func formatFileSize(s int64) (size string) {
+	if s < 1024 {
+		return fmt.Sprintf("%.2fB", float64(s)/float64(1))
+	} else if s < (1024 * 1024) {
+		return fmt.Sprintf("%.2fKB", float64(s)/float64(1024))
+	} else if s < (1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fMB", float64(s)/float64(1024*1024))
+	} else if s < (1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fGB", float64(s)/float64(1024*1024*1024))
+	} else if s < (1024 * 1024 * 1024 * 1024 * 1024) {
+		return fmt.Sprintf("%.2fTB", float64(s)/float64(1024*1024*1024*1024))
+	} else { //if s < (1024 * 1024 * 1024 * 1024 * 1024 * 1024)
+		return fmt.Sprintf("%.2fEB", float64(s)/float64(1024*1024*1024*1024*1024))
+	}
+}
